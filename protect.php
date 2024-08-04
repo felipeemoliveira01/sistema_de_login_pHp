ecd05c45832af5a23225b6671da388d68526f74f
Felipe Eduardo <79548692+felipeemoliveira01@users.noreply.github.com>
2024-08-04 01:05:31 -0300

    Delete protect.php

--- protect.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
-
-if (!isset($_SESSION['id'])) {
-    die("vaza porra😡🤬👨‍👨‍👧‍👦👉 <p><a href=\"index.php\">entra</a></p>");
-}
-?>
